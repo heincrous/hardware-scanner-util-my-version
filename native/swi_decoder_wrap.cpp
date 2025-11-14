12c15804dccc594a7ccf7ff0bfd6b2ca886786c4
heincrous <heinrichcrous123@gmail.com>
2025-11-14 15:27:00 +0200

    save: Fri 14 Nov 2025 15:27:00 SAST

--- native/swi_decoder_wrap.cpp
+++ native/swi_decoder_wrap.cpp
@@ -2,10 +2,8 @@
 #include <cstdlib>
 #include "SWIDecoder.h"
 
-extern "C" {
-    // real internal decoder
-    int sub_10011840(unsigned char* input, int inputSize, unsigned char** outRGB, int* width, int* height);
-}
+// use C++ linkage exactly matching SWIDecoder.cpp
+WiResultImage DecodeImage(unsigned char* imageData, int size);
 
 static napi_value decode_swi(napi_env env, napi_callback_info info) {
     size_t argc = 1;
@@ -16,12 +14,9 @@ static napi_value decode_swi(napi_env env, napi_callback_info info) {
     size_t cmpSize;
     napi_get_buffer_info(env, args[0], (void**)&cmpData, &cmpSize);
 
-    unsigned char* rgb = nullptr;
-    int width = 0;
-    int height = 0;
+    WiResultImage result = DecodeImage(cmpData, (int)cmpSize);
 
-    int r = sub_10011840(cmpData, (int)cmpSize, &rgb, &width, &height);
-    if (r != 0) {
+    if (result.size <= 0 || result.raw == nullptr) {
         napi_throw_error(env, nullptr, "decode failed");
         return nullptr;
     }
@@ -29,20 +24,14 @@ static napi_value decode_swi(napi_env env, napi_callback_info info) {
     napi_value out;
     napi_create_object(env, &out);
 
-    napi_value w, h;
-    napi_create_int32(env, width, &w);
-    napi_create_int32(env, height, &h);
-    napi_set_named_property(env, out, "width", w);
-    napi_set_named_property(env, out, "height", h);
+    // size only; width and height unknown
+    napi_value size;
+    napi_create_int32(env, result.size, &size);
+    napi_set_named_property(env, out, "size", size);
 
-    int dataSize = width * height * 3;
     napi_value buf;
-    unsigned char* dst;
-    napi_create_buffer_copy(env, dataSize, rgb, (void**)&dst, &buf);
-    napi_set_named_property(env, out, "data", buf);
-
-    // free memory allocated by SWIDecoder.cpp
-    free(rgb);
+    napi_create_buffer_copy(env, result.size, result.raw, nullptr, &buf);
+    napi_set_named_property(env, out, "rgb", buf);
 
     return out;
 }
