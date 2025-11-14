2f54b1cb6021407892e9e7877e8ab97e668b577a
heincrous <heinrichcrous123@gmail.com>
2025-11-14 15:21:49 +0200

    save: Fri 14 Nov 2025 15:21:49 SAST

--- native/swi_decoder_wrap.cpp
+++ native/swi_decoder_wrap.cpp
@@ -2,11 +2,8 @@
 #include "SWIDecoder.h"
 
 extern "C" {
-    WiDecmpOptions* WiCreateDecmpOptions();
-    WiRawImage* WiCreateRawImage();
-    WiCmpImage* WiCreateCmpImage();
-    int WiDecompress(WiDecmpOptions*, WiRawImage*, WiCmpImage*);
-    void WiFreeRawImageData(WiRawImage*);
+    // real internal decoder
+    int sub_10011840(unsigned char* input, int inputSize, unsigned char** outRGB, int* width, int* height);
 }
 
 static napi_value decode_swi(napi_env env, napi_callback_info info) {
@@ -18,14 +15,11 @@ static napi_value decode_swi(napi_env env, napi_callback_info info) {
     size_t cmpSize;
     napi_get_buffer_info(env, args[0], (void**)&cmpData, &cmpSize);
 
-    WiDecmpOptions* opts = WiCreateDecmpOptions();
-    WiRawImage* raw = WiCreateRawImage();
-    WiCmpImage* cmp = WiCreateCmpImage();
+    unsigned char* rgb = nullptr;
+    int width = 0;
+    int height = 0;
 
-    cmp->CmpData = cmpData;
-    cmp->Size = cmpSize;
-
-    int r = WiDecompress(opts, raw, cmp);
+    int r = sub_10011840(cmpData, (int)cmpSize, &rgb, &width, &height);
     if (r != 0) {
         napi_throw_error(env, nullptr, "decode failed");
         return nullptr;
@@ -35,18 +29,19 @@ static napi_value decode_swi(napi_env env, napi_callback_info info) {
     napi_create_object(env, &out);
 
     napi_value w, h;
-    napi_create_int32(env, raw->Width, &w);
-    napi_create_int32(env, raw->Height, &h);
+    napi_create_int32(env, width, &w);
+    napi_create_int32(env, height, &h);
     napi_set_named_property(env, out, "width", w);
     napi_set_named_property(env, out, "height", h);
 
-    size_t dataSize = raw->Width * raw->Height * 3;
+    int dataSize = width * height * 3;
     napi_value buf;
     unsigned char* dst;
-    napi_create_buffer_copy(env, dataSize, raw->Raw, (void**)&dst, &buf);
+    napi_create_buffer_copy(env, dataSize, rgb, (void**)&dst, &buf);
     napi_set_named_property(env, out, "data", buf);
 
-    WiFreeRawImageData(raw);
+    // free memory allocated by SWIDecoder.cpp
+    free(rgb);
 
     return out;
 }
