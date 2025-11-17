03ea8eb1466f008acfb5401b119105e6060a7503
heincrous <heinrichcrous123@gmail.com>
2025-11-17 12:15:22 +0200

    save: Mon 17 Nov 2025 12:15:22 SAST

--- native/swi_decoder_wrap.cpp
+++ native/swi_decoder_wrap.cpp
@@ -1,4 +1,5 @@
 #include <node_api.h>
+#include <cstdio>
 #include "SWIDecoder.h"
 
 WiDecmpOptions* WiCreateDecmpOptions();
@@ -13,20 +14,55 @@ static napi_value decode_swi(napi_env env, napi_callback_info info) {
     napi_value args[1];
     napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
 
-    unsigned char* cmpData;
-    size_t cmpSize;
+    if (argc < 1) {
+        napi_throw_error(env, nullptr, "missing argument");
+        return nullptr;
+    }
+
+    unsigned char* cmpData = nullptr;
+    size_t cmpSize = 0;
     napi_get_buffer_info(env, args[0], (void**)&cmpData, &cmpSize);
 
+    if (!cmpData || cmpSize == 0) {
+        napi_throw_error(env, nullptr, "invalid buffer");
+        return nullptr;
+    }
+
     WiDecmpOptions* opts = WiCreateDecmpOptions();
+    if (!opts) {
+        napi_throw_error(env, nullptr, "failed to allocate options");
+        return nullptr;
+    }
+
     WiRawImage* raw = WiCreateRawImage();
+    if (!raw) {
+        napi_throw_error(env, nullptr, "failed to allocate raw image");
+        return nullptr;
+    }
+
     WiCmpImage* cmp = WiCreateCmpImage();
+    if (!cmp) {
+        napi_throw_error(env, nullptr, "failed to allocate cmp image");
+        return nullptr;
+    }
 
     cmp->CmpData = cmpData;
-    cmp->Size = cmpSize;
+    cmp->Size = (int)cmpSize;
+
+    if (!cmp->CmpData || cmp->Size <= 0) {
+        napi_throw_error(env, nullptr, "invalid cmp image payload");
+        return nullptr;
+    }
 
     int r = WiDecompress(opts, raw, cmp);
+
     if (r != 0) {
         napi_throw_error(env, nullptr, "decode failed");
+        return nullptr;
+    }
+
+    if (!raw->Raw || raw->Width <= 0 || raw->Height <= 0) {
+        napi_throw_error(env, nullptr, "decoder produced invalid image");
         return nullptr;
     }
 
@@ -41,6 +77,10 @@ static napi_value decode_swi(napi_env env, napi_callback_info info) {
     napi_set_named_property(env, out, "height", h);
 
     int dataSize = raw->Width * raw->Height;
+    if (dataSize <= 0) {
+        napi_throw_error(env, nullptr, "invalid data size");
+        return nullptr;
+    }
 
     napi_value buf;
     napi_create_buffer_copy(env, dataSize, raw->Raw, nullptr, &buf);
